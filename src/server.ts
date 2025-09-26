4c3cc25f544ba894e5ccedee0e1a75aba61371a3
Aoife <imatt.lino@gmail.com>
2025-09-26 17:58:49 -0300

    continuando projeto

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { mainRoutes } from './routes/main';
+import { authRoutes } from './routes/auth';
+import { adminRoutes } from './routes/admin';
 
 const server = express();
 server.use(cors());
@@ -8,9 +11,9 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.static('public'));
 
-server.get('/ping', (req, res) => {
-    res.json({ pong: true });
-});
+server.use('/api', mainRoutes);
+server.use('/api/auth', authRoutes);
+server.use('/api/admin', adminRoutes);
 
 server.listen(4444, () => {
     console.log('TSBlog backend running...')
